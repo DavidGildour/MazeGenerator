236c61b1dcb59cbe89777d6c70c16e45c06b9368
David Gildour <davidgildour@gmail.com>
2022-01-12 22:40:52 +0100

    restructured the code

--- maze_gen/wilson/maze.py
+++ maze_gen/wilson/maze.py
@@ -1,78 +1,9 @@
 import random
-from dataclasses import dataclass
-from enum import Enum
 from itertools import chain
 
 from maze_gen import Maze
-
-
-WALL = "#"
-WALL_N = 0b1000
-WALL_E = 0b0100
-WALL_S = 0b0010
-WALL_W = 0b0001
-
-
-class Direction(Enum):
-    N = 0
-    E = 1
-    S = 2
-    W = 3
-
-
-direction_map = {
-    Direction.N: (0, -1),
-    Direction.E: (1, 0),
-    Direction.S: (0, 1),
-    Direction.W: (-1, 0),
-}
-
-
-@dataclass
-class Cell:
-    x: int
-    y: int
-    included: bool = False
-    walls: int = WALL_N | WALL_S | WALL_W | WALL_E
-    direction: Direction | None = None
-
-    @property
-    def top_row(self) -> str:
-        col_skip = self.x == 0
-        return "".join([WALL * col_skip, self.print_wall(WALL_N), WALL])
-
-    @property
-    def mid_row(self) -> str:
-        col_skip = self.x == 0
-        return "".join(
-            [self.print_wall(WALL_W) * col_skip, ".", self.print_wall(WALL_E)]
-        )
-
-    @property
-    def bot_row(self) -> str:
-        col_skip = self.x == 0
-        return "".join([WALL * col_skip, self.print_wall(WALL_S), WALL])
-
-    def connect(self, neighbouring_cell: "Cell", direction: Direction):
-        match direction:
-            case Direction.N:
-                self.walls -= WALL_N
-                neighbouring_cell.walls -= WALL_S
-            case Direction.E:
-                self.walls -= WALL_E
-                neighbouring_cell.walls -= WALL_W
-            case Direction.W:
-                self.walls -= WALL_W
-                neighbouring_cell.walls -= WALL_E
-            case Direction.S:
-                self.walls -= WALL_S
-                neighbouring_cell.walls -= WALL_N
-
-    def print_wall(self, wall: int) -> str:
-        return WALL if self.wall_exists(wall) else "."
-
-    def wall_exists(self, wall: int) -> bool:
-        return bool(self.walls & wall)
+from maze_gen.wilson.cell import Cell
+from maze_gen.wilson.direction import Direction, direction_map
 
 
 class WilsonMaze(Maze):
